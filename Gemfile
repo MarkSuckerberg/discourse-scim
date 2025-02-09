6fa92f3ec4da6fd592c168271e16a767c72b64cf
Peter Bouda <pbouda@outlook.com>
2025-02-09 11:30:28 +0000

    build: Update versions for current discourse main branch

--- Gemfile
+++ Gemfile
@@ -3,11 +3,11 @@
 source "https://rubygems.org"
 
 gem "marcel", "1.0.0", { require: false }
-gem "activestorage", "7.0.8", { require: false }
-gem "actiontext", "7.0.8", { require: false }
-gem "actionmailbox", "7.0.8", { require: false }
+gem "activestorage", "7.2.2.1", { require: false }
+gem "actiontext", "7.2.2.1", { require: false }
+gem "actionmailbox", "7.2.2.1", { require: false }
 gem "websocket-extensions", "0.1.0", { require: false }
 gem "websocket-driver", "0.6.1", { require: false }
-gem "actioncable", "7.0.8", { require: false }
-gem "rails", "7.0.8", { require: false }
+gem "actioncable", "7.2.2.1", { require: false }
+gem "rails", "7.2.2.1", { require: false }
 gem "scimitar", "2.10.0", { require: false }
--- plugin.rb
+++ plugin.rb
@@ -7,13 +7,13 @@
 # url: https://forge.libre.sh/libre.sh/discourse-scim
 
 gem "marcel", "1.0.0", { require: false }
-gem "activestorage", "7.0.8", { require: false }
-gem "actiontext", "7.0.8", { require: false }
-gem "actionmailbox", "7.0.8", { require: false }
+gem "activestorage", "7.2.2.1", { require: false }
+gem "actiontext", "7.2.2.1", { require: false }
+gem "actionmailbox", "7.2.2.1", { require: false }
 gem "websocket-extensions", "0.1.0", { require: false }
 gem "websocket-driver", "0.6.1", { require: false }
-gem "actioncable", "7.0.8", { require: false }
-gem "rails", "7.0.8", { require: false }
+gem "actioncable", "7.2.2.1", { require: false }
+gem "rails", "7.2.2.1", { require: false }
 gem "scimitar", "2.10.0", { require: false }
 
 require "scimitar"

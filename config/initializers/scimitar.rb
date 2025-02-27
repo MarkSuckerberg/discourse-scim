9a9ee4ab5516998d4ccef4ae81ca84fe5e9b7917
Mark Suckerberg <mark@suckerberg.gay>
2025-02-27 13:34:27 -0600

    can you tell I don't know how to use ruby

--- config/initializers/scimitar.rb
+++ config/initializers/scimitar.rb
@@ -1,5 +1,26 @@
 # frozen_string_literal: true
 
+class EditableUserGroups < Scimitar::Schema::Base
+  def initialize(options = {})
+    super(
+      name:            'EditableUserGroups',
+      description:     'A field to allow for the editing of user groups on the user endpoint.',
+      id:              self.class.id,
+      scim_attributes: self.class.scim_attributes
+    )
+  end
+
+  def self.id
+    'urn:ietf:params:scim:shiptest:schemas:UserGroups'
+  end
+
+  def self.scim_attributes
+    [
+      Scimitar::Schema::Attribute.new(name: "userGroups", multiValued: true, complexType: Scimitar::ComplexTypes::ReferenceGroup, mutability: "writeOnly"),
+    ]
+  end
+end
+
 Rails.application.config.to_prepare do
   Scimitar.service_provider_configuration = Scimitar::ServiceProviderConfiguration.new({
     authenticationSchemes: [
